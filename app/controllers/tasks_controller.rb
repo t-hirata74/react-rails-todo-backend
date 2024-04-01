f6f423d34af27d23c559d5e1a96eb4c43b0dc1ef
t-hirata74 <t.hirata.0463@gmail.com>
2024-04-01 21:23:51 +0900

    create Actionを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,15 @@ class TasksController < ApplicationController
     tasks = Task.all
     render json: tasks
   end
+
+  def create
+    Task.create(task_params)
+    head :created
+  end
+
+  private
+
+  def task_params
+    params.permit(:name, :is_done)
+  end
 end
